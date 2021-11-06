511737e65236f7fcfc50cbd8968b2e3a6cd31a29
Code Sandbox <hello@codesandbox.io>
2021-11-06 13:02:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9fcad7f4903fd52ed1f082a99b968740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7d1281b883f170118af15e431f775e",
+    "url": "https://pipozoft.github.io/csb-01h0r/index.html"
+  },
+  {
+    "revision": "a11b373e39f0c95520c9",
+    "url": "https://pipozoft.github.io/csb-01h0r/static/css/main.e257c922.chunk.css"
+  },
+  {
+    "revision": "d3e1216441ffe7d3ddb9",
+    "url": "https://pipozoft.github.io/csb-01h0r/static/js/2.66a39e3d.chunk.js"
+  },
+  {
+    "revision": "a11b373e39f0c95520c9",
+    "url": "https://pipozoft.github.io/csb-01h0r/static/js/main.bac33301.chunk.js"
+  },
+  {
+    "revision": "2f3bf351df2c685e0818",
+    "url": "https://pipozoft.github.io/csb-01h0r/static/js/runtime~main.cbe9750b.js"
+  }
+]);
